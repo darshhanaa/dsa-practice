9fb70d549dc35fe4b4e3297703591fefbd3315f4
Darshana Ghodasaini <darshanaghodasaini106@gmail.com>
2026-06-15 20:32:30 +0545

    Implement main function for singly linked list
    Added a main function to create and display a singly linked list.

--- linked-list/singly_linked_list.c
+++ linked-list/singly_linked_list.c
@@ -5,10 +5,31 @@ struct Node {
     int data;
     struct Node* next;
 };
+int main() {
 
+    struct Node* first = NULL;
+    struct Node* second = NULL;
+    struct Node* third = NULL;
 
+    first = (struct Node*)malloc(sizeof(struct Node));
+    second = (struct Node*)malloc(sizeof(struct Node));
+    third = (struct Node*)malloc(sizeof(struct Node));
 
+    first->data = 10;
+    first->next = second;
 
+    second->data = 20;
+    second->next = third;
+
+    third->data = 30;
+    third->next = NULL;
+
+    struct Node* temp = first;
+
+    while(temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
     printf("NULL\n");
 
     return 0;
